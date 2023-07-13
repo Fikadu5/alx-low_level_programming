328a090440d213ff8bab116300caf8280e6ceffc
Fikadu5 <fikadugetnet428@gmail.com>
2023-07-13 15:09:35 -0700

    Hello World

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,1 +1,14 @@
-#include <Z
+#include <stdio.h>
+/**
+ * main - A program that prints the size of various types in computer
+ * return; 0(success)
+ */
+int main(void)
+{
+char a;
+int b;
+long int c;
+long long int d;
+float e;
+
+printf("Size of a char: %lu byte(s)\n, (unassigned long)"
