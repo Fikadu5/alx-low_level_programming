50d5cde10fbc31665779ad6af9895b9569722011
Fikadu5 <fikadugetnet428@gmail.com>
2023-10-16 11:31:56 -0700

    update

---
+++ 0x18-dynamic_libraries/3-islower.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _islower - check if char is lowercase
+ * @c: is char to be checked
+ * Return: 1 if the char is lowercase otherwise 0.
+ */
+
+int _islower(int c)
+{
+	if(c >= 'a' && c <= 'a')
+		return (1);
+	else
+		return (0);
+}
